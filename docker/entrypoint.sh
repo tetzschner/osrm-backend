a2e6abdc989709acce49f7f088a23ad6d43f128b
tetzschner <ole.tetzschner@datalogistic.eu>
2024-04-09 08:01:47 +0200

    sync

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 OSRM_PBF_URL=${OSRM_PBF_URL:=""}
-IS_UPDATEING=${IS_UPDATEING:=false}
+OSRM_UPDATE_DATA_TAG=${OSRM_UPDATE_DATA_TAG:=OSRM_UPDATE_DATA_TAG}
 
 mkdir -p /osrm-data
 touch /osrm-data/data.osrm.starting.timestamp
 
-if [ "$IS_UPDATEING" = "true" ]; then
+touch /osrm-data/data.osrm.updated.tag
+export OSRM_UPDATED_DATA_TAG=$(</osrm-data/data.osrm.updated.tag)
+
+if [ "$OSRM_UPDATE_DATA_TAG" != "$OSRM_UPDATED_DATA_TAG" ]; then
     rm /osrm-data/* \
         && curl -L --insecure $OSRM_PBF_URL --create-dirs -o /osrm-data/data.osm.pbf \
         && osrm-extract -p /opt/car.lua /osrm-data/data.osm.pbf \
@@ -16,7 +19,8 @@ if [ "$IS_UPDATEING" = "true" ]; then
         && touch /osrm-data/data.osrm.updated.timestamp
 
     # Unset the IS_UPDATE variable
-    unset IS_UPDATEING
+    #unset IS_UPDATEING
+    echo $OSRM_UPDATE_DATA_TAG > /osrm-data/data.osrm.updated.tag
 fi
 
 touch /osrm-data/data.osrm.starting2.timestamp
