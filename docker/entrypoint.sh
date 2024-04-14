6dbf8b097b66e5584ae23e5fd1f443c164211040
tetzschner <ole.tetzschner@datalogistic.eu>
2024-04-14 20:20:49 +0200

    sync

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,29 +5,29 @@ OSRM_UPDATE_DATA_TAG=${OSRM_UPDATE_DATA_TAG:=OSRM_UPDATE_DATA_TAG}
 REPLICA_NUMBER=${REPLICA_NUMBER:=1}
 OSRM_PATH=/osrm-data/${REPLICA_NUMBER}
 
-mkdir -p /osrm-data
-touch /osrm-data/data.osrm.starting.timestamp
+mkdir -p ${OSRM_PATH}
+touch ${OSRM_PATH}/data.osrm.starting.timestamp
 
-touch /osrm-data/data.osrm.updated.tag
-export OSRM_UPDATED_DATA_TAG=$(</osrm-data/data.osrm.updated.tag)
+touch ${OSRM_PATH}/data.osrm.updated.tag
+export OSRM_UPDATED_DATA_TAG=$(<${OSRM_PATH}/data.osrm.updated.tag)
 
 echo "if ${OSRM_UPDATE_DATA_TAG} != ${OSRM_UPDATED_DATA_TAG}"
 
 if [ "$OSRM_UPDATE_DATA_TAG" != "$OSRM_UPDATED_DATA_TAG" ]; then
     echo "Updating OSRM Data now..."
-    rm /osrm-data/* \
-        && curl -L --insecure $OSRM_PBF_URL --create-dirs -o /osrm-data/data.osm.pbf \
-        && osrm-extract -p /opt/car.lua /osrm-data/data.osm.pbf \
-        && rm /osrm-data/data.osm.pbf \
-        && osrm-partition /osrm-data/data.osrm \
-        && osrm-customize /osrm-data/data.osrm \
-        && touch /osrm-data/data.osrm.updated.timestamp
+    rm ${OSRM_PATH}/* \
+        && curl -L --insecure $OSRM_PBF_URL --create-dirs -o ${OSRM_PATH}/data.osm.pbf \
+        && osrm-extract -p /opt/car.lua ${OSRM_PATH}/data.osm.pbf \
+        && rm ${OSRM_PATH}/data.osm.pbf \
+        && osrm-partition ${OSRM_PATH}/data.osrm \
+        && osrm-customize ${OSRM_PATH}/data.osrm \
+        && touch ${OSRM_PATH}/data.osrm.updated.timestamp
 
     # Unset the IS_UPDATE variable
     #unset IS_UPDATEING
-    echo $OSRM_UPDATE_DATA_TAG > /osrm-data/data.osrm.updated.tag
+    echo $OSRM_UPDATE_DATA_TAG > ${OSRM_PATH}/data.osrm.updated.tag
 fi
 
-touch /osrm-data/data.osrm.starting2.timestamp
+touch ${OSRM_PATH}/data.osrm.starting2.timestamp
 echo "Starting OSRM now..."
-osrm-routed --algorithm mld /osrm-data/data.osrm
+osrm-routed --algorithm mld ${OSRM_PATH}/data.osrm
